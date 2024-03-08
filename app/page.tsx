8205a941b02727d919bb3d7805714c8ba29c9dd1
Preston Button <prestonbutton99@gmail.com>
2024-03-08 14:13:29 +0200

    update styles/metadata

--- app/page.tsx
+++ app/page.tsx
@@ -31,13 +31,21 @@ export default function Login() {
   return (
     <div className="flex justify-center items-center min-h-screen">
         <div className="flex flex-col space-y-2 w-96">
-            <input name="email" onChange={(e) => setEmail(e.target.value)} value={email} className="border-2 border-gray-300 p-2 bg-transparent" />
+            <input 
+                name="email"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)} 
+                value={email}
+                placeholder="Email"
+                className="border-2 border-gray-500 p-2 bg-transparent focus:outline-none focus:border-gray-600 focus:bg-gray-900" 
+            />
             <input
               type="password"
               name="password"
               onChange={(e) => setPassword(e.target.value)}
               value={password}
-              className="border-2 border-gray-300 p-2 bg-transparent"
+              placeholder="Password"
+              className="border-2 border-gray-500 p-2 bg-transparent focus:outline-none focus:border-gray-600 focus:bg-gray-900"
             />
             <button onClick={handleSignIn} className="bg-gray-500 hover:bg-gray-600 text-white p-2">Sign in</button>
         </div>
