efd77372b709e34b9039c4bbd41d31ec83670c58
Preston Button <prestonbutton99@gmail.com>
2024-06-13 19:36:59 +0300

    add photos folder for sample project

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,25 @@ const Page = async () => {
   return (
     <div
       id="container"
-      className="w-svw h-svh flex flex-col items-center py-24 px-4"
+      className="w-svw h-svh flex flex-col items-center py-20 px-4"
     >
-      <h1 className="text-2xl font-bold">Danny Burrows Photography</h1>
+      <h1 className="text-2xl font-bold text-gray-700 py-4">
+        Danny Burrows Photography
+      </h1>
       <section
         id="photo_grid"
-        className="grid grid-cols-6 auto-rows-auto border w-full gap-3"
+        className="grid grid-cols-6 md:grid-cols-12 auto-rows-auto border w-full gap-3"
       >
-        <Image
-          src="https://picsum.photos/300/200"
-          alt="random image"
-          width={300}
-          height={200}
-          priority
-          className="col-span-2"
-        />
+        <div className="col-span-4 h-[200px] overflow-hidden">
+          <Image
+            src="https://picsum.photos/300/200"
+            alt="random image"
+            width={300}
+            height={200}
+            priority
+            className="w-full h-full object-cover lg:filter lg:grayscale hover:filter-none lg:hover:scale-105 transition-transform duration-300 ease-in-out"
+          />
+        </div>
       </section>
     </div>
   );
