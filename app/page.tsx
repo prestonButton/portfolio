71967187cef18ac6a7fa81b5c83c53efc7a9cb9f
Preston Button <prestonbutton99@gmail.com>
2024-06-13 19:43:41 +0300

    add other cover photos to display home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
 import Image from "next/image";
 
+// Photos Imports
+import Cover from "@/public/portfolio_photos/cover.jpg";
+import Photo1 from "@/public/portfolio_photos/1.jpg";
+import Photo2 from "@/public/portfolio_photos/2.jpg";
+import Photo3 from "@/public/portfolio_photos/3.jpg";
+import Photo4 from "@/public/portfolio_photos/4.jpg";
+import Photo5 from "@/public/portfolio_photos/5.jpg";
+import Photo6 from "@/public/portfolio_photos/6.jpg";
+import Photo7 from "@/public/portfolio_photos/7.jpg";
+import Photo8 from "@/public/portfolio_photos/8.jpg";
+import Photo9 from "@/public/portfolio_photos/9.jpg";
+import Photo10 from "@/public/portfolio_photos/10.jpg";
+import Photo11 from "@/public/portfolio_photos/11.jpg";
+import Photo12 from "@/public/portfolio_photos/12.jpg";
+import Photo13 from "@/public/portfolio_photos/13.jpg";
+import Photo14 from "@/public/portfolio_photos/14.jpg";
+import Photo15 from "@/public/portfolio_photos/15.jpg";
+import Photo16 from "@/public/portfolio_photos/16.jpg";
+import Photo17 from "@/public/portfolio_photos/17.jpg";
+import Photo18 from "@/public/portfolio_photos/18.jpg";
+
 const Page = async () => {
   return (
     <div
@@ -15,8 +36,8 @@ const Page = async () => {
       >
         <div className="col-span-4 h-[200px] overflow-hidden">
           <Image
-            src="https://picsum.photos/300/200"
-            alt="random image"
+            src={Cover}
+            alt="Cover photo for Together, Apart"
             width={300}
             height={200}
             priority
