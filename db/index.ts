314a146bb47fb6a1c621ede9abade021e4efb41b
Preston Button <prestonbutton99@gmail.com>
2024-09-04 13:58:06 +0300

    remove unnecessary node_modules

--- db/index.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-config({ path: '.env' });
-const client = postgres(process.env.DB_URL!);
-export const db = drizzle(client);
