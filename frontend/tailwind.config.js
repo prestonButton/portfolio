8fc6ea210ab5ff786970fd89efa3709f59885703
Restyled.io <commits@restyled.io>
2024-09-30 19:59:33 +0000

    Restyled by clang-format

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,29 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			colors: {
-				background: "hsl(var(--background))",
-				foreground: "hsl(var(--foreground))",
-				btn: {
-					background: "hsl(var(--btn-background))",
-					"background-hover": "hsl(var(--btn-background-hover))",
-				},
-			},
-			gridTemplateRows: {
-				'even-row': 'repeat(6, 1fr)',
-			},
-			gridTemplateColumns: {
-				'grid-cols-2': 'repeat(2, 1fr)',
-				'grid-cols-3': 'repeat(3, 1fr)',
-				'grid-cols-4': 'repeat(4, 1fr)',
-				// Add more grid sizes as needed
-			},
-		},
-	},
-	plugins: [],
+  content :
+          [
+            "./app/**/*.{js,ts,jsx,tsx,mdx}",
+            "./components/**/*.{js,ts,jsx,tsx,mdx}",
+          ],
+  theme : {
+    extend : {
+      colors : {
+        background : "hsl(var(--background))",
+        foreground : "hsl(var(--foreground))",
+        btn : {
+          background : "hsl(var(--btn-background))",
+          "background-hover" : "hsl(var(--btn-background-hover))",
+        },
+      },
+      gridTemplateRows : {
+        'even-row' : 'repeat(6, 1fr)',
+      },
+      gridTemplateColumns : {
+        'grid-cols-2' : 'repeat(2, 1fr)',
+        'grid-cols-3' : 'repeat(3, 1fr)',
+        'grid-cols-4' : 'repeat(4, 1fr)',
+        // Add more grid sizes as needed
+      },
+    },
+  },
+  plugins : [],
 };
