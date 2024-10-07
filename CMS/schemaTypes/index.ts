a97bfca3c85634aff47cfa7f215e7c33d7243bcd
Preston Button <prestonbutton99@gmail.com>
2024-10-07 17:52:47 +0300

    add imageType to cms

--- CMS/schemaTypes/index.ts
+++ CMS/schemaTypes/index.ts
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import { pictureType } from "./pictureType";
+
+export const schemaTypes = [pictureType];
---
+++ CMS/schemaTypes/pictureType.ts
@@ -0,0 +1,45 @@
+//This is the image data type
+
+import { defineField, defineType } from "sanity";
+
+export const pictureType = defineType({
+	name: "picture",
+	title: "Picture",
+	type: "document",
+	fields: [
+		defineField({
+			name: "image",
+			title: "Image",
+			type: "image",
+
+			validation: (Rule) => Rule.required(),
+		}),
+		defineField({
+			name: "title",
+			title: "Title",
+			type: "string",
+			description: "The title of the image, not required",
+		}),
+		defineField({
+			name: "caption",
+			title: "Caption",
+			type: "string",
+
+			validation: (Rule) => Rule.required(),
+		}),
+		defineField({
+			name: "alt",
+			title: "Alt Text",
+			description: "Alt text for the image. Used for accesibility",
+			type: "string",
+		}),
+		defineField({
+			name: "isCover",
+			title: "Is Cover",
+			description:
+				"If this is the cover image for the story, toggle this to on",
+			type: "boolean",
+			initialValue: false,
+		}),
+	],
+});
